74fe8b161ad83ebd54097447ff2aef3420c0b464
kaveenhyacinth <alwiskaveen@gmail.com>
2021-01-26 07:20:19 +0530

    Add protected route controllers

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -97,9 +97,34 @@ const signin = (req, res) => {
     );
 };
 
+/* Protected routes */
+
+// Saving and signin to auth object in req
+const isSignedIn = expressJwt({
+  secret: process.env.SECRET,
+  userProperty: "auth"
+});
+
+/* Custom middleware */
+
+// check whether the user is authenticated or not
+const isAuthenticated = (req, res, next) => {
+  // check the profile id which sent from the backend is equivalant with the id wich saved by isSignedIn
+  let owner = req.profile && req.auth && req.profile._id === req.auth._id;
+
+  if(!owner){
+      return res.status(403).json({
+          error: "ACCESS DENIED!"
+      });
+  }
+  next();
+}
+
 module.exports = {
   checkEmail,
   checkUsername,
   signup,
   signin,
+  isSignedIn,
+  isAuthenticated
 };
